9f42ce91c5b70722d2507f98187497f943ed1305
aoxqt4312-lang <aoxqt4312@gmail.com>
2026-05-03 04:03:24 +0500

    Create RiderService.java

---
+++ app/src/main/java/RiderService.java
@@ -0,0 +1,19 @@
+//EXAMPLE OF PATCH: java/your/pack/name/RiderService.java
+
+/*
+To start use:
+background.work.around.Start.RunService(this);
+or your context instead of "this".
+*/
+
+/*
+ATTENTION: THIS IS A DEMONSTRATIONAL PROJECT. IF YOU DO NOT OVERRIDE ALL METHODS, THE SERVICE MAY PLAY SOUND TO DEMONSTRATE ITS WORK. IN THIS EXAMPLE ALL METHODS ARE ALREADY OVERRIDDEN.
+ВНИМАНИЕ: ЭТО ДЕМОНСТРАЦИОННЫЙ ПРОЕКТ. ЕСЛИ ВЫ НЕ ПЕРЕОПРЕДЕЛИТЕ ВСЕ МЕТОДЫ, ОН МОЖЕТ ВОСПРОИЗВОДИТЬ ЗВУК ДЛЯ ДЕМОНСТРАЦИИ СВОЕЙ РАБОТЫ. В ЭТОМ ПРИМЕРЕ УЖЕ ВСЕ МЕТОДЫ ПЕРЕОПРЕДЕЛЕНЫ.
+*/
+
+package ; //your package name. (Must match the path).
+
+public class RiderService extends background.work.around.RiderService {
+
+
+}
